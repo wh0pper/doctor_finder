8b187f53a0c1e43fefd3f3d22c402ccbe44a4493
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-23 10:28:40 -0700

    fix hardcoded location and get successful api response

--- src/doctor.js
+++ src/doctor.js
@@ -1,14 +1,16 @@
 export class DoctorSearch {
   constructor(parameters) {
-    this.location = '45.5231%2C122.6765%2C25' //hardcoding Portland for now, with 25 mile range;
+    this.location = '45.523%2C-122.676%2C25' //hardcoding Portland for now, with 25 mile range;
     this.specialty = parameters.specialty;
     this.ailment = parameters.ailment;
     this.name = parameters.name;
     this.otherSearch = parameters.other;
+
   }
 
   getDoctors() {
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.otherSearch}&specialty_uid=${this.specialty}&location=${this.location}&user_key=${process.env.API_KEY}`
+    let key = 'f845c0444fa19113c09c78cfefd663f5';
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.otherSearch}&specialty_uid=${this.specialty}&location=${this.location}&user_key=${key}`
     console.log(url);
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
